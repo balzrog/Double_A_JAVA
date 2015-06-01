83480c6fc9cd4f03000eb64f8387da21099232eb
balzrog <adrien.laffargue@viacesi.fr>
2015-06-01 19:54:07 +0200

    Modification tutorial

--- test_github/src/test_github/test.java
+++ test_github/src/test_github/test.java
@@ -4,6 +4,9 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
+		System.out.println("Test for Tutorial ! ");
+
+		// Hi Github, can you see me ??
 	}
 
 }
